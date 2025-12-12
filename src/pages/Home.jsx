ae219539f07bf9189fd32c81f67e08dc61093dc4
Sparsh <sparshag2803@gmail.com>
2025-12-12 12:31:27 +0530

    completed the home page with different components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Banner from '../components/Home/Banner'
+import Hero from '../components/Home/Hero'
+import Features from '../components/Home/Features'
+import Testimonial from '../components/Home/Testimonial'
+import CallToAction from '../components/Home/CallToAction'
+import Footer from '../components/Home/Footer'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <div>
+        <Banner />
+        <Hero />
+        <Features />
+        <Testimonial />
+        <CallToAction />
+        <Footer />
+    </div>
   )
 }
 
