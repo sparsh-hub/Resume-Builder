60962b25d55a638abd7138bf83f05ffa8dd9b978
Sparsh <sparshag2803@gmail.com>
2025-12-21 16:25:34 +0530

    finally done with frontend- moving to backend

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyResumeData } from '../assets/assets'
+
+import { useParams } from 'react-router-dom'
+import Loader from '../components/Loader'
+import { ArrowLeftIcon } from 'lucide-react'
+import ResumePreview from '../components/ResumePreview'
 
 const Preview = () => {
-  return (
-    <div>Preview</div>
+
+    const { resumeId } = useParams()
+
+    const [resumeData, setResumeData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const loadResume = async () => {
+        setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        loadResume()
+    },[])
+
+  return resumeData ? (
+    <div className='bg-slate-100'>
+        <div>
+            <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accentColor} classes='py-4 bg-white' />
+        </div>
+    </div>
+  ) : (
+    isLoading ? <Loader /> : (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <p className='text-center text-6xl text-slate-400 font-medium'>Resume Not Found</p>
+            <a href='/' className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors'>
+                <ArrowLeftIcon className='mr-2 size-4' /> Go To Home Page
+            </a>
+        </div>
+    )
   )
 }
 
